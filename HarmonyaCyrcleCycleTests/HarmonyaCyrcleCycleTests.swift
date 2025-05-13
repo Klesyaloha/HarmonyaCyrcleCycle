b7037f67fbe348e85ec274fbd6a234f62e11c664
Klesya <v.klesya@outlook.fr>
2025-05-13 09:52:16 +0200

    Initial Commit

---
+++ HarmonyaCyrcleCycleTests/HarmonyaCyrcleCycleTests.swift
@@ -0,0 +1,17 @@
+//
+//  HarmonyaCyrcleCycleTests.swift
+//  HarmonyaCyrcleCycleTests
+//
+//  Created by Klesya on 5/13/25.
+//
+
+import Testing
+@testable import HarmonyaCyrcleCycle
+
+struct HarmonyaCyrcleCycleTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
